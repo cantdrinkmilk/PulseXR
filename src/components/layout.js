b22a31a06a534c17ed1281aaceb5ac334a9d8b99
Steven Tu <stu562@gmail.com>
2019-08-09 15:35:11 -0700

    integrate bulma with gatsby using template

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import './style.scss';
+import Helmet from './helmet';
+import Header from './header';
+import Midsection from './midsection';
+import Footer from './footer';
 
-import Header from "./header"
-import "./layout.css"
+const Layout = ({ children }) => (
+	<div>
+		<Helmet />
+		<Header />
+		<Midsection />
+		<Footer />
+	</div>
+);
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-
-          © {new Date().getFullYear()} by
-          {` `}
-          <a href="https://www.linkedin.com/in/steven-song-tu/">Steven Tu</a>
-          
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,7 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <p>PulseXR is a virtual reality software platform for improved surgical imaging that facilitates more precise and personalized surgical planning. 
-</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+const IndexPage = () => <Layout />;
 
-
-  </Layout>
-)
-//Link tags redirect page to the endpoint paramater
-
-export default IndexPage
+export default IndexPage;
